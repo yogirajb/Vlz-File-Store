32a24b81f3374d1fa877abdca481426723462292
yogirajb <108505047+yogirajb@users.noreply.github.com>
2025-12-16 10:47:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -47,6 +47,7 @@ AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "1800")) # Time in Second
 
 # Channel Information
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002108846764"))
+CHANNEL_ID = int(os.getenv("POST_CHANNEL_ID", "-1001234567890"))
 
 # File Caption Information
 CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", f"{script.CAPTION}")
